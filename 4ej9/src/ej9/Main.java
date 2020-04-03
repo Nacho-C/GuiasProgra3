d2c9c63fddc631ecdad8e773477838e388be94f3
luchoto720 <luchooizurieta@gmail.com>
2020-04-03 17:14:35 -0300

    ej9

---
+++ 4ej9/src/ej9/Main.java
@@ -0,0 +1,22 @@
+package ej9;
+
+public class Main
+{
+ public static void main(String[] args)
+ {
+ Base b = new Base();
+ Base e = new Extendida();
+ System.out.println(b.metodo1(e));
+ System.out.println(e.metodo1(e));
+ System.out.println(b.metodo1((Extendida) e));
+ System.out.println(e.metodo1((Extendida) e));
+ //System.out.println(b.metodo2(b));
+ //System.out.println(e.metodo2( e));
+ //System.out.println(e.metodo2((Extendida) b));
+ System.out.println(e.metodo2((Extendida) e));
+ Extendida x = (Extendida) e;
+ System.out.println(x.metodo2(b));
+ System.out.println(x.metodo2(e));
+ }
+}
+
